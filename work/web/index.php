74b4e7ad5bf7b13fdb1335d492c51faac7a06a1e
板本　耕輔 <kosuke.723723@gmail.com>
2021-05-29 23:28:50 +0900

    ドットインストール演習

--- work/web/index.php
+++ work/web/index.php
@@ -13,9 +13,28 @@ $names = [
 ];
 ?>
 
+
 <form action="result.php" method="get">
   <input type="text" name="message">
+  <?= "<br>" ?>
   <input type="text" name="username">
+  <?= "<br>" ?>
+  <textarea name="content"></textarea>
+  <?= "<br>" ?>
+  <select name="colors[]" multiple>
+    <option value="orange">orange</option>
+    <option value="pink">pink</option>
+    <option value="gold">gold</option>
+  </select>
+  <?= "<br>"; ?>
+  <label><input type="checkbox" name="colors[]" value="aaa">aaa</label>
+  <label><input type="checkbox" name="colors[]" value="bbb">bbb</label>
+  <label><input type="checkbox" name="colors[]" value="ccc">ccc</label>
+  <?= "<br>" ?>
+  <label><input type="radio" name="radio" value="one">one</label>
+  <label><input type="radio" name="radio" value="two">two</label>
+  <label><input type="radio" name="radio" value="three">three</label>
+  <?= "<br>"; ?>
   <button>Send</button>
 </form>
 
--- work/web/result.php
+++ work/web/result.php
@@ -7,13 +7,28 @@ require("../app/functions.php");
 include("../app/_parts/_header.php");
 
 // $message = filter_input(INPUT_GET, "message");
-$message = $_GET["message"];
+$message = trim($_GET["message"]);
+$content = trim($_GET["content"]);
+$content = nl2br($content);
 $username = $_GET["username"];
+$colors = !empty($_GET["colors"]) ? implode(",", $_GET["colors"]) : "None selected";
+$numbers = empty($_GET["numbers"]) ? "None selected" : implode(",", $_GET["numbers"]);
+
+// $radio = !empty($_GET["radio"]) ? $_GET["radio"] : "None selected";
+
+// "条件" ? "真の値" : "偽の値"
+$message = $message !== "" ? $message : "...";
+$content = $content !== "" ? $content : "...";
 
 
 ?>
 
+<?php print_r($message); ?>
 <p><?= h($message);?> by <?= h($username) ?> </p>
+<p><?= h($content) ?></p>
+<p><?= h($colors) ?></p>
+<p><?= h($numbers) ?></p>
+<p><?= h($radio) ?></p>
 <p><a href="index.php">Go back!</a></p>
 
 
